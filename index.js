bc2e002c19245c8fd20c67883d149880068dcea8
RuchiVerma2004 <verma.ruchi0123@gmail.com>
2024-06-21 13:25:11 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ document.addEventListener('wheel', function(event) {
   const rightContainer = document.getElementById('right');
   if (event.deltaY !== 0) {
       rightContainer.scrollTop += event.deltaY;
-      event.preventDefault(); // Prevent default scrolling
+      event.preventDefault(); 
   }
 });
+
+
+
+
+
